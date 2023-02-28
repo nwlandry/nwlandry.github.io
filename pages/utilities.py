6bcaf5f0dd7a1830a9555e331a4764455467e12e
Nicholas Landry <nicholas.landry.91@gmail.com>
2023-02-28 09:09:14 -0500

    Updates

--- pages/utilities.py
+++ pages/utilities.py
@@ -1,11 +1,13 @@
 import yaml
-from IPython.display import display, Markdown, HTML
+from IPython.display import HTML, Markdown, display
+
 
 def readable_list(_s):
-  if len(_s) < 3:
-    return ' and '.join(map(str, _s))
-  *a, b = _s
-  return f"{', '.join(map(str, a))}, and {b}"
+    if len(_s) < 3:
+        return " and ".join(map(str, _s))
+    *a, b = _s
+    return f"{', '.join(map(str, a))}, and {b}"
+
 
 def button(url, str, icon):
     icon_base = icon[:2]
@@ -14,13 +16,16 @@ def button(url, str, icon):
         {str}
     </a>"""
 
+
 def load_publication_data(path):
     yaml_data = yaml.safe_load(open(path))
     pub_strs = {"pubs": {}, "wps": {}, "theses": {}}
     for _, data in yaml_data.items():
         title_str = data["title"]
         authors = data.get("authors", ["me"])
-        authors = [a if a != "me" else "<strong>Nicholas W. Landry</strong>" for a in authors]
+        authors = [
+            a if a != "me" else "<strong>Nicholas W. Landry</strong>" for a in authors
+        ]
         author_str = readable_list(authors)
         year_str = data["year"]
 
@@ -40,7 +45,7 @@ def load_publication_data(path):
         pub_url = data.get("published_url")
         venue = data.get("venue")
         thesis_type = data.get("thesis")
-        
+
         pub_str = f'{author_str}. ({year_str}) "{title_str}."'
 
         if venue is not None:
@@ -53,23 +58,36 @@ def load_publication_data(path):
             if year_str not in pub_strs["wps"]:
                 pub_strs["wps"][year_str] = []
             pub_strs["wps"][year_str].append(
-                "<li class='list-group-item'>" + pub_str + "<br>" + " ".join(buttons) + "</li>"
+                "<li class='list-group-item'>"
+                + pub_str
+                + "<br>"
+                + " ".join(buttons)
+                + "</li>"
             )
         elif thesis_type is not None:
             if year_str not in pub_strs["theses"]:
                 pub_strs["theses"][year_str] = []
             buttons.append(button(pub_url, "Published", "ai-archive"))
             pub_strs["theses"][year_str].append(
-                "<li class='list-group-item'>" + pub_str + "<br>" + " ".join(buttons) + "</li>"
+                "<li class='list-group-item'>"
+                + pub_str
+                + "<br>"
+                + " ".join(buttons)
+                + "</li>"
             )
         else:
             if year_str not in pub_strs["pubs"]:
                 pub_strs["pubs"][year_str] = []
             buttons.append(button(pub_url, "Published", "ai-archive"))
             pub_strs["pubs"][year_str].append(
-                "<li class='list-group-item'>" + pub_str + "<br>" + " ".join(buttons) + "</li>"
+                "<li class='list-group-item'>"
+                + pub_str
+                + "<br>"
+                + " ".join(buttons)
+                + "</li>"
             )
     return pub_strs
+
 
 def load_software_data(path):
     yaml_data = yaml.safe_load(open(path))
@@ -79,10 +97,10 @@ def load_software_data(path):
         display(Markdown("(*" + data["role"] + "*)"))
         buttons = []
         if "website" in data:
-            buttons.append(button(data['website'], "Website", "bi-info-circle"))
+            buttons.append(button(data["website"], "Website", "bi-info-circle"))
         if "github" in data:
-            buttons.append(button(data['github'], "Github", "bi-github"))
+            buttons.append(button(data["github"], "Github", "bi-github"))
         if "package" in data:
-            buttons.append(button(data['package'], "Package", "bi-box-seam"))
+            buttons.append(button(data["package"], "Package", "bi-box-seam"))
         display(HTML(" ".join(buttons)))
     return
